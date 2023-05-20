59285ab4eca0a52fd0d5c719853c71793818bd9e
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 12:54:11 +0530

    final1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let api = ' http://www.omdbapi.com/?i=tt3896198&apikey=61e576a4&t=';
 let loadingstatus = false;
-loder=()=>{
+loder = () => {
   let intro = document.getElementById('intro');
   intro.style.display = 'none'
   let loder = document.getElementById('l');
@@ -11,16 +11,18 @@ loder=()=>{
     loder.classList.remove('loader');
   }
 }
-gen=()=> {
+gen = () => {
   loadingstatus = true;
   loder()
+
   let moviename = document.getElementById('moviename').value;
+
   let apiquery = api + moviename;
   console.log(apiquery);
+
   fetch(apiquery).then((response) => {
-    return response.json()
+    return response.json();
   }).then((data) => {
-    console.log(data);
     loadingstatus = false;
     loder()
     let img = document.getElementById('img');
@@ -28,7 +30,7 @@ gen=()=> {
     let p = document.getElementById('p')
     h1.innerText = data.Title;
     p.innerText = data.Plot;
-   
+    console.log(data);
     img.src = data.Poster;
   })
 }
