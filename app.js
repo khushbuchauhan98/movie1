1047db9abe49d590f7f8fe3ab37cce89903b0f80
khushbu19998@outlook.com <khushbu19998@outlook.com>
2023-05-20 12:56:39 +0530

    final1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let api = 'https://www.omdbapi.com/?i=tt3896198&apikey=c997e31e&t='
 let loadingstatus = false;
-loder = () => {
+loder=()=>{
   let intro = document.getElementById('intro');
   intro.style.display = 'none'
   let loder = document.getElementById('l');
@@ -11,7 +11,7 @@ loder = () => {
     loder.classList.remove('loader');
   }
 }
-gen = () => {
+gen=()=>{
   loadingstatus = true;
   loder()
 
@@ -20,9 +20,10 @@ gen = () => {
   let apiquery = api + moviename;
   console.log(apiquery);
 
-  fetch(apiquery).then((response) => {
+  fetch(apiquery).then((response)=>{
     return response.json()
-  }).then((data) => {
+  }).then((data)=> {
+    console.log(data);
     loadingstatus = false;
     loder()
     let img = document.getElementById('img');
@@ -30,7 +31,7 @@ gen = () => {
     let p = document.getElementById('p')
     h1.innerText = data.Title;
     p.innerText = data.Plot;
-    console.log(data);
+    
     img.src = data.Poster;
   })
 }
